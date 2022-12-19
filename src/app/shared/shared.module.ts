f10a1c13e0d6f68a71f0e77584dc4a5c94a7357d
Hasitha <ranaweerahlakshan@gmail.com>
2022-12-19 12:00:30 +0530

    created shared module + autocomplete component

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AutocompleteComponent } from './components/autocomplete/autocomplete.component';
+
+
+
+@NgModule({
+  declarations: [
+    AutocompleteComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class SharedModule { }
