ed48d2aa0d9228cfdebf94b51af3ff07226b584d
Hasitha <ranaweerahlakshan@gmail.com>
2022-12-20 15:24:45 +0530

    initial integration of auto-complete sync and async search

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'autocomplete';
+export class AppComponent implements OnInit, OnDestroy {
+
+  asyncIsLoading: boolean = false;
+  syncIsLoading: boolean = false;
+  isAsync_1: boolean = true;
+  isAsync_2: boolean = false;
+  results: any[] = [];
+
+  asyncOptions = { label_field: 'title', description_field: 'country', value_field: 'value', check_field: 'isActive', placeholder: 'Type to begin searching' };
+  syncOptions = { label_field: 'title', description_field: '', value_field: 'value', check_field: 'isActive', placeholder: 'Type to begin searching' };
+
+  asyncSuggestions: any[] = [
+    { title: '🇸🇦 Saudi Riyal', country: 'Country: SAR', value: { name: 'SAR', type: 'coin' }, isActive: true },
+    { title: 'lk Rupees', country: 'Country: Sri Lanka', value: { name: 'LK', type: 'coin' }, isActive: false },
+    { title: 'lk Rupees', country: 'Country: Sri Lanka', value: { name: 'PL', type: 'coin' }, isActive: true }
+  ];
+
+  syncSuggestions: any[] = [
+    { title: '🇸🇦 Saudi Riyal', country: 'Country: SAR', value: { name: 'SAR', type: 'coin' }, isActive: true },
+    { title: 'lk Rupees', country: 'Country: Sri Lanka', value: { name: 'LK', type: 'coin' }, isActive: false },
+    { title: 'lk Rupees', country: 'Country: Sri Lanka', value: { name: 'PL', type: 'coin' }, isActive: true },
+    { title: '🇸🇦 Saudi Riyal', country: 'Country: SAR', value: { name: 'SAR', type: 'coin' }, isActive: true },
+    { title: 'lk Rupees', country: 'Country: Sri Lanka', value: { name: 'LK', type: 'coin' }, isActive: false },
+    { title: 'lk Rupees', country: 'Country: Sri Lanka', value: { name: 'PL', type: 'coin' }, isActive: true }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+  searchByText(searchedKey: string) {
+    this.asyncIsLoading = true;
+    setTimeout(() => {
+      this.asyncSuggestions = [{ title: 'lk Rupees', country: 'Country: Sri Lanka', value: { name: 'PL', type: 'coin' }, isActive: true }];
+      this.asyncIsLoading = false;
+    }, 1000);
+  }
+
+  getResults(value: any[]) {
+    console.log(value);
+  }
+
+  ngOnDestroy(): void {
+
+  }
+
 }
--- src/app/shared/components/autocomplete/autocomplete.component.ts
+++ src/app/shared/components/autocomplete/autocomplete.component.ts
@@ -1,15 +1,136 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, EventEmitter, HostListener, Input, OnDestroy, OnInit, Output, ViewChild } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { cloneDeep } from "lodash"
+import { debounceTime, Subscription } from 'rxjs';
+import { createPopper } from '@popperjs/core';
 
 @Component({
   selector: 'app-autocomplete',
   templateUrl: './autocomplete.component.html',
   styleUrls: ['./autocomplete.component.css']
 })
-export class AutocompleteComponent implements OnInit {
+export class AutocompleteComponent implements OnInit, OnDestroy, AfterViewInit {
 
-  constructor() { }
+  searchInput: FormControl = new FormControl();
+  subscription: Subscription = new Subscription();
+  suggestionList: any[] = [];
+  selectedItems: any[] = [];
+  isDisplaySuggestions: boolean = false;
+  cursorIndex: number = -1;
 
-  ngOnInit(): void {
+  @Input() isLoading: boolean = false;
+  @Input() options: any;
+
+  @Input() set suggestions(value: any[]) {
+    if (value) {
+      this.suggestionList = cloneDeep(value);
+    }
+  }
+
+  private ASYNC: boolean = false;
+  @Input() set async(value: boolean) {
+    if (value) {
+      this.ASYNC = value;
+      this.asyncSearch();
+    }
+  }
+
+  get async() {
+    return this.ASYNC;
+  }
+
+  @Output() completeMethod: EventEmitter<string> = new EventEmitter();
+  @Output() results: EventEmitter<any[]> = new EventEmitter();
+  @ViewChild('ref') private ref: any;
+
+  @HostListener('document:click', ['$event'])
+  clickCheck(event: any) {
+    if (this.eRef.nativeElement.contains(event.target)) {
+      this.isDisplaySuggestions = true;
+    } else {
+      this.cursorIndex = -1;
+      this.isDisplaySuggestions = false;
+    }
+  }
+
+  constructor(private eRef: ElementRef) {
+    eRef = this.ref;
+  }
+
+  ngAfterViewInit(): void {
+    const reference = document.querySelector('#search') as HTMLElement;
+    const popper = document.querySelector('#drop-down') as HTMLElement;
+
+    createPopper(reference, popper, {
+      placement: 'bottom',
+      modifiers: [
+        {
+          name: 'flip',
+          options: {
+            fallbackPlacements: ['top', 'bottom'],
+          },
+        },
+      ],
+    });
+  }
+
+  ngOnInit(): void { }
+
+  getSelectedItems(item: any, isChecked: boolean) {
+    const index = this.selectedItems.findIndex(value => JSON.stringify(item) === JSON.stringify(value));
+    if (isChecked) {
+      if (index === -1) {
+        this.selectedItems.push(item);
+      }
+    } else {
+      this.selectedItems.splice(index, 1);
+    }
+    this.results.emit(this.selectedItems);
+  }
+
+
+  selectByKeys(event: any) {
+    switch (event.code) {
+      case 'ArrowUp':
+        this.isDisplaySuggestions = true;
+        if (this.cursorIndex > 0) {
+          this.cursorIndex--;
+        }
+        break;
+      case 'ArrowDown':
+        this.isDisplaySuggestions = true;
+        if (this.cursorIndex < (this.suggestionList.length - 1)) {
+          this.cursorIndex++;
+        }
+        break;
+      case 'Enter':
+        if (this.cursorIndex > -1 && this.suggestionList[this.cursorIndex][this.options.check_field]) {
+          this.suggestionList[this.cursorIndex].selected = !this.suggestionList[this.cursorIndex].selected;
+          this.getSelectedItems(this.suggestionList[this.cursorIndex], this.suggestionList[this.cursorIndex].selected);
+        }
+        break;
+      case 'Escape':
+        this.cursorIndex = -1;
+        this.isDisplaySuggestions = false;
+        break;
+      default:
+        break;
+    }
+  }
+
+  asyncSearch() {
+    this.isLoading = true;
+    this.subscription = this.searchInput.valueChanges.pipe(
+      debounceTime(1000)
+    ).subscribe((value) => {
+      this.completeMethod.emit(value);
+    });
+  }
+
+  ngOnDestroy(): void {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
