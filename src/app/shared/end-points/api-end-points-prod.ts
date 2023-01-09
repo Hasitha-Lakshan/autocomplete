87b4f6f29dd553d28e6b50337d21766468bd422b
Hasitha <ranaweerahlakshan@gmail.com>
2023-01-09 23:03:50 +0530

    Added vercel

--- src/app/shared/end-points/api-end-points-prod.ts
+++ src/app/shared/end-points/api-end-points-prod.ts
@@ -3,6 +3,6 @@ import { environment } from "src/environments/environment";
 const BASE_URL = environment.apiEndpointUrl;
 
 export const apiEndpoint = {
-    products: `${BASE_URL}/products`,
+    allProducts: `${BASE_URL}/products`,
     productsByTitle: `${BASE_URL}/products?title_like=`
 };
