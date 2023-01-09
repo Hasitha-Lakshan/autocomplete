db5466f3186dbd918d43580225761199c7e3556c
Hasitha <ranaweerahlakshan@gmail.com>
2023-01-09 22:14:56 +0530

    Updated end-points

--- src/app/shared/end-points/api-end-points-prod.ts
+++ src/app/shared/end-points/api-end-points-prod.ts
@@ -3,5 +3,6 @@ import { environment } from "src/environments/environment";
 const BASE_URL = environment.apiEndpointUrl;
 
 export const apiEndpoint = {
-    products: `${BASE_URL}/products`
+    products: `${BASE_URL}/products`,
+    productsByTitle: `${BASE_URL}/products?title_like=`
 };
